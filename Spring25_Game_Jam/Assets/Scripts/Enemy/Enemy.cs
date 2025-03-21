49e7b3241eb32b8bf695d5b6af5af1b6f76672b7
KayneTheBurr <kainejohnson63@gmail.com>
2025-03-21 12:30:31 -0700

    Spawn point and enemys

--- Spring25_Game_Jam/Assets/Scripts/Enemy/Enemy.cs
+++ Spring25_Game_Jam/Assets/Scripts/Enemy/Enemy.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    //Variables Here
+
+
+    public virtual void Start()
+    {
+        
+    }
+    public virtual void Update()
+    {
+        
+    }
 }
