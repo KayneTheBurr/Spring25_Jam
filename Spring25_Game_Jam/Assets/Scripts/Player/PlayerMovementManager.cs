82265684b32a4dd30502401463ab9c64240d39af
elbowGeese <eapricemail@gmail.com>
2025-03-21 13:35:48 -0700

    very little changes sad

--- Spring25_Game_Jam/Assets/Scripts/Player/PlayerMovementManager.cs
+++ Spring25_Game_Jam/Assets/Scripts/Player/PlayerMovementManager.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementManager : MonoBehaviour
 {
-    
+    private Vector2 horizontal;
+    private Vector2 vertical;
+
+    public float moveSpeed;
+
+    private void FixedUpdate()
+    {
+        
+    }
 }
